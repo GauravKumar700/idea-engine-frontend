e717e9afae61d2cfc0ba3049624073ada44412ee
kartik poojari <kartikpoojari29@gmail.com>
2024-04-19 19:32:50 +0530

    top scrolling added

--- src/components/PublicRelations.jsx
+++ src/components/PublicRelations.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
 function PublicRelations() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="container-fluid">
       <div className="row">
