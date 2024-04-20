85d7d9c02dc6486444761a187ac7c853e38d0c1e
Gaurav Kumar <gauravkumar7004515@gmail.com>
2024-04-20 11:48:57 +0530

    Code By Kartik

--- src/Mycomponents/Stepper.jsx
+++ src/Mycomponents/Stepper.jsx
@@ -3,15 +3,15 @@ import { Button } from "react-bootstrap";
 
 function Stepper({ steps }) {
   const [activeStep, setActiveStep] = useState(0);
-  const [step, setStep] = useState(0)
+  const [step, setStep] = useState(1)
 
   const handleNext = () => {
-    setStep(step + 1)
+    setStep((step) => step + 1 )
     setActiveStep((prevStep) => prevStep + 1);
   };
 
   const handleBack = () => {
-    setStep(step - 1)
+    setStep((step) => step - 1 )
     setActiveStep((prevStep) => prevStep - 1);
   };
 
@@ -29,7 +29,7 @@ function Stepper({ steps }) {
       <div className='d-flex justify-content-center'>
         <Button disabled={activeStep === 0} onClick={handleBack} variant="outline-primary" className=' mx-2'>Back</Button>
         {step !== 6 && <Button disabled={activeStep === steps.length - 1} onClick={handleNext} variant="outline-primary" className=' mx-2'>Next</Button>}
-        {step === 6 && <Button disabled={activeStep === steps.length - 1} onClick={handleNext} variant="outline-primary" className=' mx-2'>Generate Pdf</Button>}
+        {step === 6 && <Button variant="outline-primary" className=' mx-2'>Generate Pdf</Button>}
       </div>
     </div>
   );
